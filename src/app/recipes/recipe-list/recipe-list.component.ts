77e00c4a97d9afcf1694333a43d1c16e7451829c
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-05-25 17:18:46 -0700

    Wk 3 - data binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,9 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg')
   ];
   
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -7,9 +7,16 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
+
+  ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient:Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
